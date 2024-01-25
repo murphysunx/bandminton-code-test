f25a0d63d487d4775bd2dc84851694b4161ff461
Murphy Sun <murphysunx@gmail.com>
2024-01-25 20:29:18 +1100

    Tets(match-generator): add two test cases

--- apps/badminton-backend/src/app/domains/match-generator/index.spec.ts
+++ apps/badminton-backend/src/app/domains/match-generator/index.spec.ts
@@ -4,8 +4,11 @@ describe('Matcher', () => {
 
   function validateMatchGenerator(matcher: MatchGenerator) {
     const rankings = matcher.rank();
+    // console.log(rankings);
     const matches = matcher.generateNextRound();
-    expect(matches.length).toBe(2);
+    // console.log(matches);
+
+    expect(matches.length).toBe(Object.keys(matcher.playerDict).length / 2);
     for (const match of matches) {
       expect(match).toBeDefined();
       expect(match.length).toBe(2);
@@ -81,7 +84,7 @@ describe('Matcher', () => {
     expect(ranks[3].secondaryPoints).toBe(-10);
   });
 
-  it('should be able to use secondary points as a tie breaker', () => {
+  it('should be able to generate next round', () => {
     const matcher = new MatchGenerator([
       [
         {
@@ -105,6 +108,174 @@ describe('Matcher', () => {
           winner: 'Player 2',
           loser: 'Player 4',
           points: 5,
+        },
+      ],
+    ]);
+    validateMatchGenerator(matcher);
+  });
+
+  it('should be able to generate next round 1', () => {
+    const matcher = new MatchGenerator([
+      [
+        {
+          winner: 'Player 1',
+          loser: 'Player 2',
+          points: 1,
+        },
+        {
+          winner: 'Player 3',
+          loser: 'Player 4',
+          points: 2,
+        },
+        {
+          winner: 'Player 5',
+          loser: 'Player 6',
+          points: 3,
+        },
+        {
+          winner: 'Player 7',
+          loser: 'Player 8',
+          points: 4,
+        },
+        {
+          winner: 'Player 9',
+          loser: 'Player 10',
+          points: 5,
+        },
+        {
+          winner: 'Player 11',
+          loser: 'Player 12',
+          points: 6,
+        },
+        {
+          winner: 'Player 13',
+          loser: 'Player 14',
+          points: 7,
+        },
+        {
+          winner: 'Player 15',
+          loser: 'Player 16',
+          points: 8,
+        },
+        {
+          winner: 'Player 17',
+          loser: 'Player 18',
+          points: 9,
+        },
+        {
+          winner: 'Player 19',
+          loser: 'Player 20',
+          points: 10,
+        },
+      ],
+    ]);
+    validateMatchGenerator(matcher);
+  });
+
+  it('should be able to generate next round 2', () => {
+    const matcher = new MatchGenerator([
+      [
+        {
+          winner: 'Player 1',
+          loser: 'Player 2',
+          points: 1,
+        },
+        {
+          winner: 'Player 3',
+          loser: 'Player 4',
+          points: 2,
+        },
+        {
+          winner: 'Player 5',
+          loser: 'Player 6',
+          points: 3,
+        },
+        {
+          winner: 'Player 7',
+          loser: 'Player 8',
+          points: 4,
+        },
+        {
+          winner: 'Player 9',
+          loser: 'Player 10',
+          points: 5,
+        },
+        {
+          winner: 'Player 11',
+          loser: 'Player 12',
+          points: 6,
+        },
+        {
+          winner: 'Player 13',
+          loser: 'Player 14',
+          points: 7,
+        },
+        {
+          winner: 'Player 15',
+          loser: 'Player 16',
+          points: 8,
+        },
+        {
+          winner: 'Player 17',
+          loser: 'Player 18',
+          points: 9,
+        },
+        {
+          winner: 'Player 19',
+          loser: 'Player 20',
+          points: 10,
+        },
+      ],
+      [
+        {
+          winner: 'Player 19',
+          loser: 'Player 17',
+          points: 1,
+        },
+        {
+          winner: 'Player 20',
+          loser: 'Player 1',
+          points: 2,
+        },
+        {
+          winner: 'Player 18',
+          loser: 'Player 3',
+          points: 3,
+        },
+        {
+          winner: 'Player 15',
+          loser: 'Player 13',
+          points: 4,
+        },
+        {
+          winner: 'Player 16',
+          loser: 'Player 5',
+          points: 5,
+        },
+        {
+          winner: 'Player 14',
+          loser: 'Player 7',
+          points: 6,
+        },
+        {
+          winner: 'Player 9',
+          loser: 'Player 11',
+          points: 7,
+        },
+        {
+          winner: 'Player 10',
+          loser: 'Player 2',
+          points: 8,
+        },
+        {
+          winner: 'Player 12',
+          loser: 'Player 4',
+          points: 9,
+        },
+        {
+          winner: 'Player 8',
+          loser: 'Player 6',
+          points: 10,
         },
       ],
     ]);
